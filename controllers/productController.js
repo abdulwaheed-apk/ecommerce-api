3e3da3bde5cd0236c225501f40548b20a7566032
Abdul Waheed Mughal <ab.waheedkashmiri@gmail.com>
2024-07-27 08:59:01 +0300

    product updates

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,91 @@
+import { Product } from '../models/productModel.js'
+
+//@route /api/admin/product/all
+//@method GET To get all product
+//@access private only admin
+export const getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find({})
+        if (products.length === 0) {
+            res.status(404).json({ message: 'Products not found' })
+        }
+        res.status(200).json({ message: 'All products', products })
+    } catch (error) {
+        res.status(500).json({ message: `Server Error ${error.message}` })
+    }
+}
 //@route /api/admin/product
 //@method POST To create product
 //@access private only admin
-export const createProduct = async (req, res) => {}
+export const createProduct = async (req, res) => {
+    const {
+        name,
+        description,
+        product_image,
+        qty_in_stock,
+        sku,
+        price,
+        sale_price,
+        category,
+        variation,
+        color,
+        product_images,
+    } = req.body
+
+    try {
+        const product = await Product.create({
+            name,
+            description,
+            product_image,
+            qty_in_stock,
+            sku,
+            price,
+            sale_price: sale_price ? sale_price : 0,
+            category,
+            variation,
+            color,
+            product_images: product_images,
+        })
+        console.log('product created', product)
+        return res.status(201).json({
+            message: 'Product created successfully',
+            product,
+        })
+    } catch (error) {
+        res.status(500).json({ message: `Server Error ${error.message}` })
+    }
+}
+//@route /api/admin/product/:id
+//@method DELETE To delete product
+//@access private only admin
+export const deleteProduct = async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    try {
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' })
+        }
+        const deletedProduct = await Product.findByIdAndDelete(req.params.id)
+        res.status(200).json({ message: 'Product Deleted Successfully' })
+    } catch (error) {
+        res.status(500).json({ message: `Server Error ${error.message}` })
+    }
+}
+//@route /api/admin/product/:id
+//@method PUT/PATCH To update product
+//@access private only admin
+export const updateProduct = async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    res.status(200).json({
+        message: 'Got it',
+        id: req.params.id,
+        q: `Do you want to update ${product.name}`,
+    })
+}
+//@route /api/admin/product/:category
+//@method GET To get by category product
+//@access private only admin
+export const getProductByCategory = async (req, res) => {
+    res.send(`Category name is ${req.params.category}`)
+}
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,22 @@
 import { Router } from 'express'
 import { authenticate, authorizeAdmin } from '../middlewares/auth.js'
-import { createProduct } from '../controllers/productController.js'
+import {
+    createProduct,
+    deleteProduct,
+    getAllProducts,
+    getProductByCategory,
+} from '../controllers/productController.js'
 
 const router = Router()
 
 router.post('/product', authenticate, authorizeAdmin, createProduct)
+router.get('/product/all', authenticate, authorizeAdmin, getAllProducts)
+router.delete('/product/:id', authenticate, authorizeAdmin, deleteProduct)
+router.get(
+    '/product/:category',
+    authenticate,
+    authorizeAdmin,
+    getProductByCategory
+)
 
 export default router
